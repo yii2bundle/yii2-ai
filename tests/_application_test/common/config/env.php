9e328e5d39056cd691250d6feca629d79f1499c7
Vitaliy <theyamshikov@yandex.ru>
2019-02-02 19:06:49 +0600

    fix

--- tests/_application_test/common/config/env.php
+++ tests/_application_test/common/config/env.php
@@ -5,11 +5,11 @@ return [
 		'commands' => [
 			[
 				'class' => 'yii2lab\\app\\domain\\commands\\RunBootstrap',
-				'app' => 'vendor/yii2lab/yii2-app/tests/_application_test\\common',
+				'app' => 'vendor/yii2rails/yii2-app/tests/_application_test\\common',
 			],
 			[
 				'class' => 'yii2lab\\app\\domain\\commands\\RunBootstrap',
-				'app' => 'vendor/yii2lab/yii2-app/tests/_application_test\\console',
+				'app' => 'vendor/yii2rails/yii2-app/tests/_application_test\\console',
 			],
 			[
 				'class' => 'yii2lab\\app\\domain\\commands\\ApiVersion',
@@ -21,82 +21,82 @@ return [
 		'filters' => [
 			[
 				'class' => 'yii2lab\\app\\domain\\filters\\config\\LoadConfig',
-				'app' => 'vendor/yii2lab/yii2-app/tests/_application_test\\common',
+				'app' => 'vendor/yii2rails/yii2-app/tests/_application_test\\common',
 				'name' => 'main',
 				'withLocal' => true,
 			],
 			[
 				'class' => 'yii2lab\\app\\domain\\filters\\config\\LoadConfig',
-				'app' => 'vendor/yii2lab/yii2-app/tests/_application_test\\console',
+				'app' => 'vendor/yii2rails/yii2-app/tests/_application_test\\console',
 				'name' => 'main',
 				'withLocal' => true,
 			],
 			[
 				'class' => 'yii2lab\\app\\domain\\filters\\config\\LoadModuleConfig',
-				'app' => 'vendor/yii2lab/yii2-app/tests/_application_test\\common',
+				'app' => 'vendor/yii2rails/yii2-app/tests/_application_test\\common',
 				'name' => 'modules',
 				'withLocal' => true,
 			],
 			[
 				'class' => 'yii2lab\\app\\domain\\filters\\config\\LoadModuleConfig',
-				'app' => 'vendor/yii2lab/yii2-app/tests/_application_test\\console',
+				'app' => 'vendor/yii2rails/yii2-app/tests/_application_test\\console',
 				'name' => 'modules',
 				'withLocal' => true,
 			],
 			[
 				'class' => 'yii2lab\\app\\domain\\filters\\config\\LoadConfig',
-				'app' => 'vendor/yii2lab/yii2-app/tests/_application_test\\common',
+				'app' => 'vendor/yii2rails/yii2-app/tests/_application_test\\common',
 				'name' => 'routes',
 				'withLocal' => true,
 				'assignTo' => 'components.urlManager.rules',
 			],
 			[
 				'class' => 'yii2lab\\app\\domain\\filters\\config\\LoadConfig',
-				'app' => 'vendor/yii2lab/yii2-app/tests/_application_test\\console',
+				'app' => 'vendor/yii2rails/yii2-app/tests/_application_test\\console',
 				'name' => 'routes',
 				'withLocal' => true,
 				'assignTo' => 'components.urlManager.rules',
 			],
 			[
 				'class' => 'yii2lab\\app\\domain\\filters\\config\\LoadConfig',
-				'app' => 'vendor/yii2lab/yii2-app/tests/_application_test\\common',
+				'app' => 'vendor/yii2rails/yii2-app/tests/_application_test\\common',
 				'name' => 'params',
 				'withLocal' => true,
 				'assignTo' => 'params',
 			],
 			[
 				'class' => 'yii2lab\\app\\domain\\filters\\config\\LoadConfig',
-				'app' => 'vendor/yii2lab/yii2-app/tests/_application_test\\console',
+				'app' => 'vendor/yii2rails/yii2-app/tests/_application_test\\console',
 				'name' => 'params',
 				'withLocal' => true,
 				'assignTo' => 'params',
 			],
 			[
 				'class' => 'yii2lab\\domain\\filters\\LoadDomainConfig',
-				'app' => 'vendor/yii2lab/yii2-app/tests/_application_test\\common',
+				'app' => 'vendor/yii2rails/yii2-app/tests/_application_test\\common',
 				'name' => 'domains',
 				'withLocal' => true,
 			],
 			[
 				'class' => 'yii2lab\\app\\domain\\filters\\config\\LoadConfig',
-				'app' => 'vendor/yii2lab/yii2-app/tests/_application_test\\common',
+				'app' => 'vendor/yii2rails/yii2-app/tests/_application_test\\common',
 				'name' => 'install',
 			],
 			[
 				'class' => 'yii2lab\\app\\domain\\filters\\config\\LoadConfig',
-				'app' => 'vendor/yii2lab/yii2-app/tests/_application_test\\console',
+				'app' => 'vendor/yii2rails/yii2-app/tests/_application_test\\console',
 				'name' => 'install',
 			],
 			[
 				'class' => 'yii2lab\\app\\domain\\filters\\config\\LoadConfig',
-				'app' => 'vendor/yii2lab/yii2-app/tests/_application_test\\common',
+				'app' => 'vendor/yii2rails/yii2-app/tests/_application_test\\common',
 				'name' => 'test',
 				'withLocal' => true,
 				'isEnabled' => true,
 			],
 			[
 				'class' => 'yii2lab\\app\\domain\\filters\\config\\LoadConfig',
-				'app' => 'vendor/yii2lab/yii2-app/tests/_application_test\\console',
+				'app' => 'vendor/yii2rails/yii2-app/tests/_application_test\\console',
 				'name' => 'test',
 				'withLocal' => true,
 				'isEnabled' => true,
@@ -105,7 +105,7 @@ return [
 			'yii2lab\\app\\domain\\filters\\config\\FixValidationKeyInTest',
 			'yii2lab\\app\\domain\\filters\\config\\SetAppId',
 			'yii2lab\\app\\domain\\filters\\config\\SetPath',
-			'yii2module\\offline\\domain\\filters\\IsOffline',
+			//'yii2module\\offline\\domain\\filters\\IsOffline',
 		],
 	],
 	'url' => [
@@ -144,20 +144,6 @@ return [
 		'static' => [
 			'publicPath' => '@frontend/web/',
 			'domain' => 'http://qr.yii/',
-		],
-		'wsdl' => [
-			'domain' => 'http://www.test.wooppay.com/api/wsdl',
-			'payment_hash' => 'Q8nFbQeU236zYQmHDq5vHVqeQBgjNmu9sTCVtEP7hL7p6kKC2vJc66pUGbrAhD3G',
-			'user' => [
-				[
-					'login' => 'QRPayMerchant',
-					'password' => 'A12345678a',
-				],
-				[
-					'login' => 'QRPaySub',
-					'password' => 'A12345678a',
-				],
-			],
 		],
 	],
 	'mode' => [
