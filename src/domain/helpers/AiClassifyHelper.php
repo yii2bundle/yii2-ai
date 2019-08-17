e776f2393bfa28bc9b73bbbdbeafb0cb584e8e54
Vitaliy <theyamshikov@yandex.ru>
2019-08-17 22:24:42 +0600

    feat: upgrade

--- src/domain/helpers/AiClassifyHelper.php
+++ src/domain/helpers/AiClassifyHelper.php
@@ -7,11 +7,11 @@ use NlpTools\FeatureFactories\FeatureFactoryInterface;
 use NlpTools\Models\MultinomialNBModelInterface;
 use NlpTools\Tokenizers\TokenizerInterface;
 use NlpTools\Tokenizers\WhitespaceTokenizer;
-use NlpTools\Models\FeatureBasedNB;
 use NlpTools\Documents\TrainingSet;
 use NlpTools\Documents\TokensDocument;
 use NlpTools\FeatureFactories\DataAsFeatures;
 use NlpTools\Classifiers\MultinomialNBClassifier;
+use yii2lab\ai\domain\models\FeatureBasedNB;
 
 class AiClassifyHelper {
 
@@ -29,20 +29,10 @@ class AiClassifyHelper {
 	 * @var ClassifierInterface
 	 */
 	protected $classifier;
-	
-	/**
-	 * @var array
-	 */
-	protected $classes = [];
-	
-	/**
-	 * @var TrainingSet
-	 */
-	protected $trainingSet;
 
 	public function __construct()
     {
-        $this->trainingSet = new TrainingSet();
+        $this->model = new FeatureBasedNB;
     }
 
     public function classify($value) {
@@ -50,128 +40,41 @@ class AiClassifyHelper {
         return $prediction;
     }
 
-    public function trainingSet($training) {
-        foreach ($training as $trainingDocument) {
-            $this->trainingSet->addDocument($trainingDocument[0], new TokensDocument($this->tokenizer->tokenize($trainingDocument[1])));
-        }
-        return $this->trainingSet;
-    }
-
-    public function setModel(array $data)
-    {
-        $this->model->setData($data);
-    }
-
-    public function getModel() : array
-    {
-        return $this->model->getData();
-    }
-
-    public function createClassifier($trainingSet) {
-        // will hold the features
+    public function setModel($model) {
+        $this->model->setData($model);
         $features = new DataAsFeatures();
-
-        // train our Naive Bayes Model
-        $this->model = new MyFeatureBasedNB();
-        $this->model->train($features, $trainingSet);
-        // *************** Classify ***************
-        // init our Naive Bayes Class using the features and our model
         $this->classifier = new MultinomialNBClassifier($features, $this->model);
-        //d($this->model->getData());
-        return $this->classifier;
-    }
-	
-}
-
-
-class MyFeatureBasedNB extends FeatureBasedNB {
-
-    public function setData(array $data)
-    {
-        $this->priors = $data['priors'];
-        $this->condprob = $data['condprob'];
-        $this->unknown = $data['unknown'];
     }
 
-    public function getData() : array
+    public function getModel()
     {
-        return [
-            'priors' => $this->priors,
-            'condprob' => $this->condprob,
-            'unknown' => $this->unknown,
-        ];
+        return (array) $this->model->getData();
+    }
+
+    public function train($training) {
+        $trainingSet = new TrainingSet();
+        foreach ($training as $trainingDocument) {
+            $trainingSet->addDocument($trainingDocument[0], new TokensDocument($this->tokenizer->tokenize($trainingDocument[1])));
+        }
+        $features = new DataAsFeatures();
+        $this->model->train($features, $trainingSet);
+        $this->classifier = new MultinomialNBClassifier($features, $this->model);
     }
 
 }
 
 /*
 // *************** Example ***************
-$training = [
-    ['usa', 'new york is a hell of a town'],
-    ['usa', 'the statue of liberty'],
-    ['usa', 'new york is in the united states'],
-    ['usa', 'the white house is in washington'],
-    ['uk', 'london is in the uk'],
-    ['uk', 'the big ben is in london'],
-];
-
-$testSet = [
-    ['usa', 'i want to see the statue of liberty'],
-    ['usa', 'this is a picture of the white house'],
-    ['usa', 'where in washington'],
-    ['uk', 'i saw the big ben yesterday'],
-    ['uk', 'i went to london to visit a friend'],
-];
+$training = include(ROOT_DIR . DS . 'vendor/yii2bundle/yii2-ai/src/domain/data/example1/training.php');
+$testSet = include(ROOT_DIR . DS . 'vendor/yii2bundle/yii2-ai/src/domain/data/example1/test.php');
+$expected = include(ROOT_DIR . DS . 'vendor/yii2bundle/yii2-ai/src/domain/data/example1/expected.php');
+$model = include(ROOT_DIR . DS . 'vendor/yii2bundle/yii2-ai/src/domain/data/example1/model.php');
 
 $ai = new AiClassifyHelper;
 $ai->tokenizer = new WhitespaceTokenizer;
-$trainingSet = $ai->trainingSet($training);
-$ai->createClassifier($trainingSet);
+$ai->train($training);
+//$ai->setModel($model);
 
-//d($ai->getModel());
-
-$result = [];
-
-foreach ($testSet as $testDocument) {
-    list($expectedClass, $value) = $testDocument;
-    $prediction = $ai->classify($value);
-    $result[] = [
-        $value,
-        $expectedClass,
-        $prediction,
-    ];
-    if($prediction != $expectedClass) {
-        throw new \Exception('Error prediction!');
-    }
-}
-
-$expected = [
-    [
-        'i want to see the statue of liberty',
-        'usa',
-        'usa',
-    ],
-    [
-        'this is a picture of the white house',
-        'usa',
-        'usa',
-    ],
-    [
-        'where in washington',
-        'usa',
-        'usa',
-    ],
-    [
-        'i saw the big ben yesterday',
-        'uk',
-        'uk',
-    ],
-    [
-        'i went to london to visit a friend',
-        'uk',
-        'uk',
-    ],
-];
-
-d($result == $expected);
+$result = NeuroTestHelper::testClassify($testSet, $ai);
+d($result == $expected, 1);
  */
